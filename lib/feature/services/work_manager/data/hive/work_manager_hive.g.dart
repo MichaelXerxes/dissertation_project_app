e7690c8956eca84227501f16ed06b3d7e1060dfe
MichaelHiszpanski <michal.hiszpanski@thoc.dev>
2024-10-07 16:42:49 +0100

    work manager hive

---
+++ lib/feature/services/work_manager/data/hive/work_manager_hive.g.dart
@@ -0,0 +1,56 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'work_manager_hive.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class MeetingAdapter extends TypeAdapter<Meeting> {
+  @override
+  final int typeId = 3;
+
+  @override
+  Meeting read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Meeting(
+      eventName: fields[0] as String,
+      eventDescription: fields[1] as String,
+      startDate: fields[2] as DateTime,
+      finishDate: fields[3] as DateTime,
+      backgroundColor: fields[4] as Color,
+      isAllDay: fields[5] as bool,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Meeting obj) {
+    writer
+      ..writeByte(6)
+      ..writeByte(0)
+      ..write(obj.eventName)
+      ..writeByte(1)
+      ..write(obj.eventDescription)
+      ..writeByte(2)
+      ..write(obj.startDate)
+      ..writeByte(3)
+      ..write(obj.finishDate)
+      ..writeByte(4)
+      ..write(obj.backgroundColor)
+      ..writeByte(5)
+      ..write(obj.isAllDay);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is MeetingAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
