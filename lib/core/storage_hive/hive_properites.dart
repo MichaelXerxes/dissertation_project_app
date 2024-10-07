7e2b5064a44130be3a725d8dbdeda20ff660196d
MichaelHiszpanski <michal.hiszpanski@thoc.dev>
2024-10-07 16:42:31 +0100

    work manager hive

--- lib/core/storage_hive/hive_properites.dart
+++ lib/core/storage_hive/hive_properites.dart
@@ -2,11 +2,23 @@ class HiveProperties {
   static const int databaseVersion = 1;
 
   static const int toDoListID = 2;
+  static const int workManagerID = 3;
 }
 
 class HiveToDoListProperties {
   static const String TO_DO_LIST_DATA_BOX = "TO_DO_LIST_DATA_BOX";
   static const String TO_DO_LIST_DATA_KEY = "TO_DO_LIST_DATA_KEY";
+  static const int id = 0;
+  static const int title = 1;
+  static const int content = 2;
+  static const int dateTimeAdded = 3;
+  static const int expiredDate = 4;
+  static const int priority = 5;
+}
+
+class HiveWorkManagerProperties {
+  static const String TO_WORK_MANAGER_DATA_BOX = "TO_WORK_MANAGER_DATA_BOX";
+  static const String TO_WORK_MANAGER_DATA_KEY = "TO_WORK_MANAGER_DATA_KEY";
   static const int id = 0;
   static const int title = 1;
   static const int content = 2;
---
+++ lib/feature/services/work_manager/data/hive/work_manager_hive.dart
@@ -0,0 +1,45 @@
+import 'package:dissertation_project_app/core/storage_hive/hive_properites.dart';
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:intl/intl.dart';
+
+part 'work_manager_hive.g.dart';
+
+@HiveType(typeId: HiveProperties.workManagerID)
+class Meeting {
+  @HiveField(0)
+  final String eventName;
+
+  @HiveField(1)
+  final String eventDescription;
+
+  @HiveField(2)
+  final DateTime startDate;
+
+  @HiveField(3)
+  final DateTime finishDate;
+
+  @HiveField(4)
+  final Color backgroundColor;
+
+  @HiveField(5)
+  final bool isAllDay;
+
+  Meeting({
+    required this.eventName,
+    required this.eventDescription,
+    required this.startDate,
+    required this.finishDate,
+    required this.backgroundColor,
+    required this.isAllDay,
+  });
+
+  @override
+  String toString() {
+    final DateFormat formatter = DateFormat('yyyy-MM-dd HH:mm:ss');
+    return 'Meeting(eventName: $eventName, eventDescription: $eventDescription, '
+        'startDate: ${formatter.format(startDate)}, '
+        'finishDate: ${formatter.format(finishDate)}, '
+        'isAllDay: $isAllDay)';
+  }
+}
